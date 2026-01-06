bc225c2b852953d4c0644c142e531bdc94ea8558
Hyperlynx <dev@hyperlynx.dev>
2026-01-06 12:45:31 -0600

    Fix activating for the Plasma edit menu thing

--- contents/code/main.js
+++ contents/code/main.js
@@ -1,7 +1,7 @@
 workspace.windowAdded.connect(spreadWindowToEmptyDesktop);
 
 function spreadWindowToEmptyDesktop(window) {
-    if(window.normalWindow && !window.specialWindow && !window.transient && window.moveable && !window.hidden) {
+    if(window.normalWindow && !window.specialWindow && !window.transient && window.moveable && !window.hidden && !window.skipSwitcher && !window.skipPager) {
         for (const desktop of workspace.desktops) {
             if(noWindowsOnDesktop(desktop)) {
                 window.desktops = [desktop];
@@ -20,3 +20,4 @@ function noWindowsOnDesktop(desktop) {
     }
     return true;
 }
+
