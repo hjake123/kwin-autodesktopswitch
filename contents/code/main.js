5c2ccc095a8503ec1e212510426501ecb24fbba8
Hyperlynx <dev@hyperlynx.dev>
2026-01-06 11:22:10 -0600

    Add the kwin script

---
+++ contents/code/main.js
@@ -0,0 +1,22 @@
+workspace.windowAdded.connect(spreadWindowToEmptyDesktop);
+
+function spreadWindowToEmptyDesktop(window) {
+    if(window.normalWindow && !window.specialWindow && !window.transient && window.moveable && !window.hidden) {
+        for (const desktop of workspace.desktops) {
+            if(noWindowsOnDesktop(desktop)) {
+                window.desktops = [desktop];
+                workspace.currentDesktop = desktop;
+                break;
+            }
+        }
+    }
+}
+
+function noWindowsOnDesktop(desktop) {
+    for(const window of workspace.stackingOrder) {
+        if(window.normalWindow && window.desktops.includes(desktop)) {
+            return false;
+        }
+    }
+    return true;
+}
